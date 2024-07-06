c01b10314d0869dbbb9da4924318fb1041bd6856
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2024-07-06 19:26:40 +0300

    added about

---
+++ app/about/about.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+about_blueprint = Blueprint('about', __name__, template_folder='templates/about/', static_folder='static')
+
+
+@about_blueprint.route('/about')
+def about():
+    title = 'about'
+    return render_template('about.html', title=title)
